03aff4a884fadef1147b86b78ae92df1641c645c
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-05-04 16:19:07 +0100

    Added simple bash script to convert a spreadsheet into TEI XML.

---
+++ convert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MEDIA_TYPE=`file -b --mime-type "$1"`
+
+case "$MEDIA_TYPE" in
+  "application/vnd.ms-excel")
+    TEMP_DIR=`mktemp -d`
+    unzip "$1" -d $TEMP_DIR > /dev/null
+    saxon office-open-xml/main.xsl -it:main url=${TEMP_DIR}/
+    rm -rf $TEMP_DIR
+    ;;
+  *)
+    echo "Could not determine file type"
+    exit 1
+    ;;
+esac
+    
